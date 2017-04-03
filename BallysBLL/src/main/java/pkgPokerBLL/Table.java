82cf9c109e6bb081a5af49b88c83854c2a1241c2
Lorry <Lorry@LORRYLISA.lan>
2017-04-03 19:42:57 -0400

    create new game and table

--- BallysBLL/src/main/java/pkgPokerBLL/Table.java
+++ BallysBLL/src/main/java/pkgPokerBLL/Table.java
@@ -1,29 +1,37 @@
 package pkgPokerBLL;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.UUID;
 
 public class Table {
 
 	private UUID TableID;
 	
-	//	Change this from ArrayList to HashMap.
-	private ArrayList<Player> TablePlayers = new ArrayList<Player>();
+	
+	HashMap<Player, UUID> TablePlayers = new HashMap<Player, UUID>();
 	
 	public Table() {
 		super();
-		TableID = UUID.randomUUID();
+		setTableID(UUID.randomUUID());
 	}
 	
 	public Table AddPlayerToTable(Player p)
-	{
-		//TODO: Add a player to the table (should be pretty easy)		
+	{	
+		TablePlayers.put(p, TableID);
 		return this;
 	}
 	
 	public Table RemovePlayerFromTable(Player p)
 	{
-		//TODO: Remove a player from the table
+		TablePlayers.remove(p, TableID);
 		return this;
+	}
+
+	public UUID getTableID() {
+		return TableID;
+	}
+
+	public void setTableID(UUID tableID) {
+		TableID = tableID;
 	}
 }
